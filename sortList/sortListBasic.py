f3afe556a7cce4cbc0c351cab2cdd3762ad87576
pxpie <pxpietrzak@outlook.com>
2022-10-31 11:25:14 +0100

    Modify sortList.py

--- sortList/sortListBasic.py
+++ sortList/sortListBasic.py
@@ -1,4 +1,25 @@
 
 
 if __name__ == '__main__':
-    pass
+    '''Create C list by addind A and B
+        Sort new list without build in functions
+    '''
+    A = [1, 3, 4, 5, 6, 7, 7]
+    B = [1, 2, 3, 8, 9, 5]
+
+    C = A + B 
+
+    moveitem = True
+
+    while(moveitem):
+        moveitem = False
+        for i in range(len(C)-1):
+            o1 = C[i]
+            o2 = C[i+1]
+            
+            if o2 < o1:
+                C[i] = o2
+                C[i+1] = o1
+                moveitem = True
+    print(C)
+            
